f86f3ee3c61a17a9dea72126d87cffa8957a44a1
Deathlymad <meyerphilip96@googlemail.com>
2016-01-16 22:24:51 +0100

    Removed most of the example stuffs

---
+++ app/src/main/java/com/wbgapp/webteam/wbgapp/NewsFeed.java
@@ -0,0 +1,10 @@
+package com.wbgapp.webteam.wbgapp;
+
+/**
+ * Created by Deathlymad on 16.01.2016.
+ */
+public class NewsFeed {
+
+    public  static NewsFeed feed;
+
+}
--- app/src/main/java/com/wbgapp/webteam/wbgapp/WBGApp.java
+++ app/src/main/java/com/wbgapp/webteam/wbgapp/WBGApp.java
@@ -34,7 +34,7 @@ public class WBGApp extends AppCompatActivity
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
@@ -80,17 +80,11 @@ public class WBGApp extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_news) {
 
-        } else if (id == R.id.nav_slideshow) {
+        }else  if(id == R.id.nav_settings){
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        }else if (id == R.id.nav_send) {
 
         }
 
